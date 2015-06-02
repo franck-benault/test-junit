1b48618155fdc424a64b8e889dabc6b1aa31144d
franck-benault <franck-benault@orange.fr>
2015-06-02 23:11:24 +0200

    cleaning code temporary folder

--- src/test/java/net/franckbenault/junit/s03/junitrules/TemporaryFolderRulesTestCase.java
+++ src/test/java/net/franckbenault/junit/s03/junitrules/TemporaryFolderRulesTestCase.java
@@ -1,14 +1,9 @@
 package net.franckbenault.junit.s03.junitrules;
 
-import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.IOException;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -18,21 +13,7 @@ public class TemporaryFolderRulesTestCase {
 	@Rule
 	public TemporaryFolder folder = new TemporaryFolder();
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
 
 	@Test
 	public void testCreateFile() throws IOException {
