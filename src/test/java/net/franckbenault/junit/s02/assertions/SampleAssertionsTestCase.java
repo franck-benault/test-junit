324624958e7cb354ba87164d636b6609fcdf58da
franck-benault <franck-benault@orange.fr>
2015-06-07 21:58:29 +0200

    refactoring tests with assertion

--- src/test/java/net/franckbenault/junit/s02/assertions/SampleAssertionsTestCase.java
+++ src/test/java/net/franckbenault/junit/s02/assertions/SampleAssertionsTestCase.java
@@ -4,36 +4,17 @@ import static org.junit.Assert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class SampleAssertionsTestCase {
 
-	private int[] ints1 = {1,3};
-	private int[] ints2 = {1,3};
-	
-	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
 	public void t01AssertArrayEqualsTest() {
+		
+		int[] ints1 = {1,3};
+		int[] ints2 = {1,3};
+
+		
 		assertArrayEquals(ints1, ints2);
 		assertArrayEquals("explanation",ints1, ints2);
 	}
