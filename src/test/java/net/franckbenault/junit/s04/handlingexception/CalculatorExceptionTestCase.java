b0689b63ca672ec0db0be7a740c99d1f8bbbb58f
franck-benault <franck-benault@orange.fr>
2015-06-18 23:39:26 +0200

    add handling exception with junit rule

--- src/test/java/net/franckbenault/junit/s04/handlingexception/CalculatorExceptionTestCase.java
+++ src/test/java/net/franckbenault/junit/s04/handlingexception/CalculatorExceptionTestCase.java
@@ -5,11 +5,15 @@ import static org.junit.Assert.fail;
 import static org.junit.Assert.assertEquals;
 import net.franckbenault.junit.Calculator;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class CalculatorExceptionTestCase {
 
-
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 
 	@Test
 	public void testDivide01TryCatch() {
@@ -25,6 +29,16 @@ public class CalculatorExceptionTestCase {
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testDivide02Exception() {
+		
+		Calculator.divide(1, 0);
+	
+	}
+	
+	@Test
+	public void testDivide03JunitRule() {
+		
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage(CoreMatchers.containsString("Could not divide by 0"));
 		
 		Calculator.divide(1, 0);
 	
